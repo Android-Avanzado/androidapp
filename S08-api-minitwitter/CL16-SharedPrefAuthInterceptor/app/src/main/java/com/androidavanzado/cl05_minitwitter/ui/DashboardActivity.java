b4ff57135e975d43e5ed77bc442741f73f53e8f7
Miguel Campos <camposmiguel@gmail.com>
2019-03-06 00:51:47 +0100

    S08-MiniTwitter

---
+++ S08-api-minitwitter/CL16-SharedPrefAuthInterceptor/app/src/main/java/com/androidavanzado/cl05_minitwitter/ui/DashboardActivity.java
@@ -0,0 +1,51 @@
+package com.androidavanzado.cl05_minitwitter.ui;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.androidavanzado.cl05_minitwitter.R;
+import com.androidavanzado.cl05_minitwitter.common.Constantes;
+import com.androidavanzado.cl05_minitwitter.common.SharedPreferencesManager;
+
+public class DashboardActivity extends AppCompatActivity {
+
+    private TextView mTextMessage;
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            switch (item.getItemId()) {
+                case R.id.navigation_home:
+                    mTextMessage.setText(R.string.title_home);
+                    return true;
+                case R.id.navigation_dashboard:
+                    mTextMessage.setText(R.string.title_dashboard);
+                    return true;
+                case R.id.navigation_notifications:
+                    mTextMessage.setText(R.string.title_notifications);
+                    return true;
+            }
+            return false;
+        }
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dashboard);
+
+        mTextMessage = (TextView) findViewById(R.id.message);
+        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+
+
+    }
+
+}
