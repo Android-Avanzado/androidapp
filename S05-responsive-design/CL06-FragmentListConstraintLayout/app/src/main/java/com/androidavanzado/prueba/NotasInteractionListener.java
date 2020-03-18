0542da773f532c1133d32ed603478157b9fc1f8f
Miguel Campos <camposmiguel@gmail.com>
2020-03-18 20:52:26 +0100

    Actualización Android Studio 3.6 y sección 15 - Google Play

--- S05-responsive-design/CL06-FragmentListConstraintLayout/app/src/main/java/com/androidavanzado/prueba/NotasInteractionListener.java
+++
@@ -1,7 +0,0 @@
-package com.androidavanzado.prueba;
-
-public interface NotasInteractionListener {
-    void editNotaClick(Nota nota);
-    void eliminaNotaClick(Nota nota);
-    void favoritaNotaClick(Nota nota);
-}
---
+++ S05-responsive-design/CL06RecyclerViewFragmentList/app/src/main/java/com/androidavanzado/prueba/MainActivity.java
@@ -0,0 +1,31 @@
+package com.androidavanzado.prueba;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+    Button btnLogin;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        getSupportActionBar().hide();
+
+        btnLogin = findViewById(R.id.buttonLogin);
+
+        // Definir el evento click sobre el botón de login
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(MainActivity.this, NotasActivity.class);
+                startActivity(i);
+            }
+        });
+    }
+}
