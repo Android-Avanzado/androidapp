fdfa1fd4d2b82c1677092517b3aba7d202eb6a76
Miguel Campos <camposmiguel@gmail.com>
2020-04-25 19:18:25 +0200

    Se actualiza sección 05

---
+++ S05-responsive-design/CL03Guias/app/src/main/java/com/androidavanzado/cl01_constraintlayout/MainActivity.java
@@ -0,0 +1,14 @@
+package com.androidavanzado.cl01_constraintlayout;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
